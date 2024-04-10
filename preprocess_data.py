4b82df0924ccfdb5a50d6108ff2555e1198abf08
zhangxd <285249802@qq.com>
2024-04-10 16:18:06 +0800

    source code and samples

---
+++ preprocess_data.py
@@ -0,0 +1,101 @@
+
+import os
+import shutil
+import tempfile
+import glob
+import random
+import numpy as np
+import ants
+import nibabel as nib
+import SimpleITK as sitk
+# use  "source ~/.bashrc " to activate fsl
+# conda install fsleyes
+# export PATH=$PATH:/home/zhangxd/anaconda3/envs/py380/bin/
+mni152_path = './inputs/MNI152_T1_1mm.nii.gz'
+
+def preprocess_fsl(params):
+    #fsl_anat -i /home/zhangxd/Projects/Data/brain/FCD2-public/data/sub-00146/anat/sub-00075_acq-iso08_T1w.nii.gz
+    #  -o /home/zhangxd/Projects/Data/brain/FCD2-public/prep-ants/fcd/sub-00146 --nononlinreg
+    # robunstfov -i xx -r xx (output) -m xx
+    # 1) reorient2std; 2) robust fov; 3) register flair to t1; 4) register all to mni152
+    data_dir = os.path.join(params['data_dir'], 'raw')
+    save_dir = os.path.join(params['data_dir'], 'fsl')
+    os.makedirs(save_dir, exist_ok=True)
+
+    t1_files = sorted( glob.glob(os.path.join(data_dir, "*/t1.nii.gz"), recursive=True) )
+    fl_files = sorted( glob.glob(os.path.join(data_dir, "*/flair.nii.gz"), recursive=True) )
+
+    sub_count = len(t1_files)
+    for i in range(sub_count):
+        name = t1_files[i].split('/')[-2]
+        sub_save_dir = os.path.join(save_dir, name)
+        os.makedirs(sub_save_dir, exist_ok=True)
+            
+        sub_t1_path = t1_files[i]
+        sub_fl_path = fl_files[i]
+
+        #========================================================
+        #origin to std
+        sub_t1_std = os.path.join(sub_save_dir, 't1_std.nii.gz')
+        mat_ori2std = os.path.join(sub_save_dir, 'ori2std.mat')
+        strcmd = 'fslreorient2std -m {} {} {}'.format(mat_ori2std, sub_t1_path, sub_t1_std)
+        os.system(strcmd)
+        
+        #robust fov
+        sub_t1_fov = os.path.join(sub_save_dir, 't1_fov.nii.gz')
+        mat_fov2std = os.path.join(sub_save_dir, 'fov2std.mat')
+        strcmd = 'robustfov -i {} -r {} -m {}'.format(sub_t1_std, sub_t1_fov, mat_fov2std)
+        os.system(strcmd)
+
+        sub_t1_reg = os.path.join(sub_save_dir, 't1_reg.nii.gz')
+        mat_fov2mni = os.path.join(sub_save_dir, 'fov2mni.mat')
+        strcmd = 'flirt -in {} -ref {} -out {} -omat {} -dof 12 -cost corratio  \
+                -bins 256 -interp trilinear \
+                -searchrx -90 90 -searchry -90 90 -searchrz -90 90'.format(sub_t1_fov, mni152_path, sub_t1_reg, mat_fov2mni)
+        os.system(strcmd)
+
+        #convert_xfm -omat ${T1}_nonroi2roi.mat -inverse ${T1}_roi2nonroi.mat
+        mat_std2fov = os.path.join(sub_save_dir, 'std2fov.mat')
+        strcmd = 'convert_xfm -omat {} -inverse {}'.format(mat_std2fov, mat_fov2std)
+        os.system(strcmd)
+
+        mat_ori2fov = os.path.join(sub_save_dir, 'ori2fov.mat')
+        strcmd = 'convert_xfm -omat {} -concat {} {}'.format(mat_ori2fov, mat_std2fov, mat_ori2std)
+        os.system(strcmd)
+
+        mat_ori2mni = os.path.join(sub_save_dir, 'ori2mni.mat')
+        strcmd = 'convert_xfm -omat {} -concat {} {}'.format(mat_ori2mni, mat_fov2mni, mat_ori2fov)
+        os.system(strcmd)
+
+        strcmd = 'flirt -in {} -ref {} -out {} -init {} -interp trilinear -applyxfm'.format(sub_t1_path, mni152_path, sub_t1_reg, mat_ori2mni)
+        os.system(strcmd)
+
+        #========================================================
+        #reg flair to t1-original, then to mni152
+        sub_fl_reg0 = os.path.join(sub_save_dir, 'flair_reg0.nii.gz')
+        mat_fl2t1 = os.path.join(sub_save_dir, 'mat_fl2t1.mat')
+        strcmd = 'flirt -in {} -ref {} -out {} -omat {} -dof 6 -cost mutualinfo  \
+                -bins 256 -interp trilinear \
+                -searchrx -90 90 -searchry -90 90 -searchrz -90 90'.format(sub_fl_path, sub_t1_path, sub_fl_reg0, mat_fl2t1)
+        os.system(strcmd)
+
+        sub_fl_reg = os.path.join(sub_save_dir, 'flair_reg.nii.gz')
+        strcmd = 'flirt -in {} -ref {} -out {} -init {} -interp trilinear -applyxfm'.format(sub_fl_reg0, sub_t1_reg, sub_fl_reg, mat_ori2mni)
+        os.system(strcmd)
+
+        #========================================================
+        gt_files = sorted( glob.glob(os.path.join(data_dir, name, "gt.nii.gz"), recursive=True) )
+        if len(gt_files)==0:
+            continue
+
+        sub_gt_path = gt_files[0]
+        sub_gt_reg0 = os.path.join(sub_save_dir, 'gt_reg0.nii.gz')
+        strcmd = 'flirt -in {} -ref {} -out {} -init {} -interp nearestneighbour -applyxfm'.format(sub_gt_path, sub_t1_path, sub_gt_reg0, mat_fl2t1)
+        os.system(strcmd)
+
+        sub_gt_reg = os.path.join(sub_save_dir, 'gt_reg.nii.gz')
+        strcmd = 'flirt -in {} -ref {} -out {} -init {} -interp nearestneighbour -applyxfm'.format(sub_gt_reg0, sub_t1_reg, sub_gt_reg, mat_ori2mni)
+        os.system(strcmd)
+
+    params['data_dir'] = save_dir
+    return params
---
+++ seg_fcd_test.py
@@ -0,0 +1,122 @@
+import os
+import shutil
+import tempfile
+import glob
+import random
+import datetime
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1" 
+device_ids = [0,1]
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import numpy as np 
+import nibabel as nib
+import torch
+
+from monai.inferers import sliding_window_inference
+from get_model import get_model
+from get_transforms import get_test_transforms
+
+from monai.data import (
+    DataLoader,
+    CacheDataset,
+    load_decathlon_datalist,
+    decollate_batch,
+)
+from monai.transforms import (
+    SaveImaged,
+)
+from preprocess_data import preprocess_fsl
+
+#==================================================
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+params = dict()
+
+params['base_dir'] = './outputs'
+params['model_name'] = './pretrained/model.pth'
+params['data_dir'] = './inputs/fsl'
+params['model_type'] = 'MS_DSA_NET' 
+params['sa_type'] = 'parallel' #parallel, spatial, channel
+params['chans_in']   = 2
+params['chans_out']  = 2
+params['feature_size'] = 16 
+params['project_size'] = 64 #dsa projection size
+params['patch_size'] = [128]*3 
+params['num_workers'] = 2
+params['seq'] = 't1+t2' #'t1+t2' #t1, t2, t1+t2
+params['chans_in'] = 2
+params['date'] = datetime.date.today()
+
+os.makedirs(params['base_dir'], exist_ok=True)
+
+def test(model, params):
+
+    pretrain = params['model_name']
+    if os.path.exists(pretrain):
+        model.load_state_dict( torch.load(pretrain) )
+        print('pretrained model ' + pretrain + ' loaded')
+    else:
+        print('no pretrained model found')
+
+    model.to(device=device)
+    model.eval()
+
+    test_transform, post_transform = get_test_transforms(params)
+    test_dict = get_data(params)
+    test_ds = CacheDataset(data=test_dict, transform=test_transform, cache_num=4, cache_rate=1, num_workers=params['num_workers'])
+    test_loader = DataLoader(test_ds, batch_size=1, shuffle=False, num_workers=params['num_workers'], pin_memory=True)
+
+    epoch_iterator_val = tqdm(test_loader, desc="test (X / X Steps) (dice=X.X)", dynamic_ncols=True)
+    
+    metrics = dict()
+    with torch.no_grad():
+        idx = 0
+        for batch in epoch_iterator_val:
+            idx += 1
+
+            original_affine = batch["image_meta_dict"]["affine"][0].numpy()
+            img_name = batch["image_meta_dict"]["filename_or_obj"][0].split("/")[-2]
+            img_name = img_name.split('.')[0]
+
+            val_inputs = batch["image"].to(device)
+            val_outputs = sliding_window_inference(val_inputs, params['patch_size'], 2, model, overlap = 0.25)
+            batch['pred'] = val_outputs
+ 
+            output_dir = os.path.join(params['save_dir'], img_name)
+            os.makedirs(output_dir, exist_ok=True)
+
+            batch = [post_transform(i) for i in decollate_batch(batch)]
+            #val_preds = val_inputs['pred'].cpu().numpy().astype(np.uint8)
+            output_name = os.path.join(output_dir, 'seg.nii.gz')
+            SaveImaged(keys="pred", output_dir=output_dir, output_postfix="seg", resample=False, separate_folder=False)(batch[0])
+
+
+def get_data(params):
+    t1_files = sorted( glob.glob(os.path.join(params["data_dir"], "*/t1_reg.nii.gz"),  recursive=True) )
+    fl_files = sorted( glob.glob(os.path.join(params["data_dir"], "*/flair_reg.nii.gz"), recursive=True) )
+
+    data_dict = []
+    for t1_f, fl_f in zip(t1_files, fl_files):
+        if params['seq'] == 't1':
+            data_dict.append({'image':t1_f})
+        elif params['seq'] == 't2':
+            data_dict.append({'image':fl_f})
+        else:
+            data_dict.append({'image':[t1_f, fl_f]})
+    
+    return data_dict
+
+def main(params):
+    
+    #params = preprocess_fsl(params)
+
+    model, params = get_model(params)
+
+    params['save_dir'] = os.path.join(params['base_dir'] , str(params['date']))
+    os.makedirs(params['save_dir'], exist_ok=True)
+    
+    test(model, params)
+    
+
+if __name__ == '__main__':
+    main(params=params)
